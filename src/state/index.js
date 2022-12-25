65f31cbcf59262f62be4b0eaf59021e0fcf1f1c6
BlueWinds <blue@everblue.info>
2022-12-25 12:47:36 -0800

    Clean up dependencies

--- src/state/index.js
+++ src/state/index.js
@@ -1,5 +1,4 @@
 import {createStore} from 'redux'
-import { Peer } from 'peerjs'
 
 import reducer from './reducer'
 
@@ -7,15 +6,6 @@ import reducer from './reducer'
 export default function state() {
   const initialState = localStorage.automatonCrusade ? JSON.parse(localStorage.automatonCrusade) : {}
   const store = createStore(reducer, initialState)
-
-  const peerId = localStorage.automatonCrusadePeerID || Math.random()
-  localStorage.automatonCrusadePeerID = peerId
-
-  const peer = new Peer(peerId)
-
-  peer.on('connection', connection => {
-
-  })
 
   // If we have a roster already stored, we want to re-load it, in case the parsing logic has changed since it
   // was loaded in.
